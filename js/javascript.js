871c1613c56c2cdd2055e821d00bf7fe9eb44985
Karen Yodico <karen.yodico@gmail.com>
2023-01-13 15:42:01 -0800

    change of project layout

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,22 @@
-window.onload = function() {
+function getAndSetProject2Values() {
+  const question1Input = document.getElementbyID("question1Input").value;
+  const question2Input = document.getElementbyID("question2Input").value;
+  const question3Input = document.getElementbyID("question3Input").value;
+  const question4Input = document.getElementbyID ("question4Input").value;
+
+  document.querySelector("span#question1").innerText = question1Input;
+  document.querySelector("span#question2").innerText = question2Input;
+  document.querySelector("span#question3").innerText = question3Input;
+  document.querySelector("span#question4").innerText = question4Input;
+}   
+function setFormSubmissionEventHandler() {
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
-
-    const question1Input = document.getElementbyID("question1Input").value;
-    const question2Input = document.getElementbyID("question2Input").value;
-    const question3Input = document.getElementbyID("question3Input").value;
-    const question4Input = document.getElementbyID ("qustion4Input").value;
-
-    document.querySelector("span#question1").innerText = "answer 1";
-    document.querySelector("span#question2").innerText = "answer 2";
-    document.querySelector("span#question3").innerText = "answer 3";
-    document.querySelector("span#question4").innterText = "answer 4";
-   
-    document.querySelector("div#aboutme").removeAttribute("class");
-
     event.preventDefault();
-  };  
-};
-  
+    getAndSetProjectValues();
+    document.querySelector("div#aboutme").removeAttribute("class");
+   }
+}
+window.onload = function() {
+  setFormSubmissionEventHandler();
+}; 
