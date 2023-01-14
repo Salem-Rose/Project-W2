abf212b1438126f4fde9ef47a2d69a8ecd0bc69b
Karen Yodico <karen.yodico@gmail.com>
2023-01-13 18:57:22 -0800

    tried another way to get the program to work

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,31 @@
-function getAndSetProject2Values() {
-  const question1Input = document.getElementbyID("question1Input").value;
-  const question2Input = document.getElementbyID("question2Input").value;
-  const question3Input = document.getElementbyID("question3Input").value;
-  const question4Input = document.getElementbyID ("question4Input").value;
-
-  document.querySelector("span#question1").innerText = question1Input;
-  document.querySelector("span#question2").innerText = question2Input;
-  document.querySelector("span#question3").innerText = question3Input;
-  document.querySelector("span#question4").innerText = question4Input;
-}   
-function setFormSubmissionEventHandler() {
-  let form = document.querySelector("form");
-  form.onsubmit = function(event) {
-    event.preventDefault();
-    getAndSetProjectValues();
-    document.querySelector("div#aboutme").removeAttribute("class");
-   }
+function hideResultsAndError() {
+  document.getElementById("error-message").setAttribute("class","hidden");
+  document.getElementById("letter").setAttribute("class", "hidden");
+  document.getElementById("color").setAtttribute("class", "hidden");
+  document.getElementById("mining").setAttribute("class", "hidden");
+  document.getElementById("skiing").setAttribute("class", "hidden");
+  document.getElementById("snake").setAttribute("class", "hidden");
 }
+ 
 window.onload = function() {
-  setFormSubmissionEventHandler();
+  hideResultsAndError();
+
+  document.querySelector("form").onsubmit = function(event) {
+    event.preventDefault();
+    hideResultsAndError();
+    form.onsubmit = function(event) {
+    
+    if (letter && color && mining && skiing & snake) {
+      if (letter === color && color === mining && mining === skiing && skiing ==== snake) {
+        result = "mining";
+      } else if (letter === color && color === mining || skiing === snake) {
+        result = "skiing";
+      } else {
+        result = "snake";
+      } else {
+        document.getElementbyId = "error-message";
+      }
+    }
+  };
 }; 
+
