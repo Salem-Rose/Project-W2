8560da9ce7a1a6e14ea611de321f7abba00e054b
Karen Yodico <karen.yodico@gmail.com>
2023-01-13 20:55:24 -0800

    show changes made to attempt to fix error

--- js/javascript.js
+++ js/javascript.js
@@ -1,31 +1,30 @@
-function hideResultsAndError() {
-  document.getElementById("error-message").setAttribute("class","hidden");
-  document.getElementById("letter").setAttribute("class", "hidden");
-  document.getElementById("color").setAtttribute("class", "hidden");
-  document.getElementById("mining").setAttribute("class", "hidden");
-  document.getElementById("skiing").setAttribute("class", "hidden");
-  document.getElementById("snake").setAttribute("class", "hidden");
-}
- 
-window.onload = function() {
-  hideResultsAndError();
-
-  document.querySelector("form").onsubmit = function(event) {
-    event.preventDefault();
-    hideResultsAndError();
-    form.onsubmit = function(event) {
-    
-    if (letter && color && mining && skiing & snake) {
-      if (letter === color && color === mining && mining === skiing && skiing ==== snake) {
-        result = "mining";
-      } else if (letter === color && color === mining || skiing === snake) {
-        result = "skiing";
-      } else {
-        result = "snake";
-      } else {
-        document.getElementbyId = "error-message";
-      }
+    function hideResultsAndEror() {
+      document.getElementById("error-message").setAttribute("class", "hidden");
+      document.getElementById("ruby").setAttribute("class", "hidden");
+      document.getElementById("swift").setAttribute("class", "hidden");
+      document.getElemenetById("python").setAttribute("class", "hidden");
     }
-  };
-}; 
+    
+    window.onload = function() {
+      hideResultsAndEror();
+
+      document.querySelector("form").onsubmit = function(event) {
+        event.preventDefault();
+        hideResultsAndEror();
+        const age = parseInt(document.querySelector)("input#age").value;
+        const height = parseInt(document.querySelector)("input#height").value;
+
+        if (age && height) {
+          if ( age >=20 && height 58) {
+            document.getElementById("ruby").removeAttribute("class");
+            document.getElementById("swift").removeAttribute("class");
+          } esle if >=25 && height === 58) {
+            document.getElementById("ruby").removeAttribute("class");
+            document.getElementById("python").removeAttribute("class");
+          } esle if (age >= 20) {
+            document.getElementById("swift").removeAttribute("class");
+            document.getElementById("python").removeAttribute("class");
+          }
+        };
+      };
 
