25e4dd502e939427b45ab09185f1ce4c4e52ae7b
Karen Yodico <karen.yodico@gmail.com>
2023-01-21 17:55:45 -0800

    ReadMe should now have a link to github link

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
- Program Language Suggester
+## Program Language Suggester
  
  by Karen Yodico
 
@@ -6,22 +6,28 @@
 
 ## Technologies Used
 
-* _Visual Studio Code_
-* _Terminal_
-* _HTML_
-* _CSS_
+* Visual Studio Code
+* Terminal
+* HTML
+* CSS
 
 ## Description
 
 A fun questionnare on how to help you pick your first programming language.
 
-
 ## Known Bugs
 
 * No known bugs
 
+## GitHub Link
+https://github.com/Salem-Rose/Project-W2
+
+## Setup/Installation Requirement
+* Download Repository 
+* Open projectw2.html with a browser that supports HTML.
+
 ## License
 
-{MIT}
+If you have any issues running this program you can contact me at karen.yodico@gmail.com.
 
 Copyright (c) January 13, 2023 - Karen Yodico
--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,31 @@
     form.onsubmit = function(event) {
       event.preventDefault();
 
-    let programSelector = document.getElementById("ruby");
-      programSelector.setAttribute("class", "hidden");
-    let programSelector2 = document.getElementById("swift"); 
-      programSelector2.setAttribute("class", "hidden");
-    let programSelector3 = document.getElementById("python");
-      programSelector3.setAttribute("class", "hidden")
+      let rubyElement = document.getElementById("ruby");
+      rubyElement.setAttribute("class", "hidden");
+      let swiftElement = document.getElementById("swift"); 
+      swiftElement.setAttribute("class", "hidden");
+      let pythonElement = document.getElementById("python");
+      pythonElement.setAttribute("class", "hidden");
+      let sorryElement = document.getElementById("sorry");
+      sorryElement.setAttribute("class", "hidden");
 
-    const age = parseInt(document.querySelector("input#age").value);
-    const height = parseInt(document.querySelector("input#height").value);
+      const age = parseInt(document.querySelector("input#age").value);
+      const height = parseInt(document.querySelector("input#height").value);
+      const mining = document.querySelector("input#mining").value;
+      const skiing = document.querySelector("input#skiing").value;
+      const snake = document.querySelector("input#snake").value;
 
-
-    if (age > 18 && height > 58) {
-      programSelector.removeAttribute("class");
-    } else if (age < 18 && height < 58) {
-      programSelector2.removeAttribute("class"); 
-    } else if (age === 18 && height === 58) {
-      programSelector3.removeAttribute("class");
+    if (age > 18 && height > 58 && mining.toLowerCase() === "yes") {
+      rubyElement.removeAttribute("class");
+      } else if (age < 18 && height < 58 && skiing.toLowerCase() === "yes") {
+      swiftElement.removeAttribute("class"); 
+      } else if (age === 18 && height === 58 && snake.toLowerCase() === "yes") {
+      pythonElement.removeAttribute("class");
+      } else {
+      sorryElement.removeAttribute("class");
+      }
     }
    };
-  };
+  
    
